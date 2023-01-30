2e2b34866904bf7aaa0fcbcef396fc93bbcc9bf0
Vasiliy <evtuth@gmail.com>
2023-01-30 08:37:14 +0300

    added sort maxmin 2array

---
+++ zadacha54/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54. Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void MaxMinMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(1) - 1; z++)
+            {
+                if (matrix[i, z] < matrix[i, z + 1])
+                {
+                    int temp = matrix[i, z + 1];
+                    matrix[i, z + 1] = matrix[i, z];
+                    matrix[i, z] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+int m = 4;
+int n = 5;
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+Console.WriteLine("Исходный массив:");
+PrintArray(matrix);
+
+Console.WriteLine();
+MaxMinMatrix(matrix);
+Console.WriteLine("Измененный массив:");
+PrintArray(matrix);

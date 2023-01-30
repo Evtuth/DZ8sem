161e1b51c8b1cbdd4d07a7ccdc894c246ff4b0f1
Vasiliy <evtuth@gmail.com>
2023-01-30 08:45:15 +0300

    added 3array

---
+++ zadacha60/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2х2х2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void FillArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = new Random().Next(1, 100);
+            }
+
+        }
+
+    }
+}
+
+void PrintArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write(matrix[i, j, z]);
+                Console.Write($" ({i}, {j}, {z})" + "\t");
+            }
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число z:");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = new int[m, n, z];
+FillArray(matrix);
+PrintArray(matrix);
